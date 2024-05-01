d9094d812485ca0f81600a603f785667058a2df7
Shammir Baig <mshammirbaig@gmail.com>
2024-05-01 15:41:02 +0530

    added the action creators

---
+++ src/Actions/jobActions.js
@@ -0,0 +1,35 @@
+import * as actionTypes from './jobActionTypes';
+
+export const fetchJobs = (limit, offset) => async (dispatch) => {
+  dispatch({ type: actionTypes.FETCH_JOBS_REQUEST });
+
+  try {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "limit": limit,
+        "offset": offset
+      })
+    };
+
+    const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions);
+    const data = await response.json();
+    console.log(data);
+    dispatch({ type: actionTypes.FETCH_JOBS_SUCCESS, payload: data.jdList });
+  } catch (error) {
+    dispatch({ type: actionTypes.FETCH_JOBS_FAILURE, payload: error.message });
+  }
+};
+
+export const setVisibleJobs = (visibleJobs) => ({
+  type: actionTypes.SET_VISIBLE_JOBS,
+  payload: visibleJobs
+});
+
+export const setStartIndex = (startIndex) => ({
+  type: actionTypes.SET_START_INDEX,
+  payload: startIndex
+});
