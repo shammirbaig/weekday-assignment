3887e2c7766558e33b79c0345d2aabdd9ca85b33
Shammir Baig <mshammirbaig@gmail.com>
2024-05-01 15:35:59 +0530

    improved the ui for the job cards

--- src/Components/Jobcard.js
+++ src/Components/Jobcard.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import "../../src/App.css";
+import { spacing } from '@mui/system';
 import { Card, CardContent, Typography, Button } from '@mui/material';
 
 
@@ -9,16 +11,20 @@ const JobCard = ({ job, charactersLimit }) => {
         setExpanded(prev => !prev)
     }
     return (
-        <Card>
+        <Card sx={{ borderRadius: "20px" }}>
             <CardContent>
-                <Typography variant="h6" gutterBottom>{job.jobRole}</Typography>
-                <Typography variant="subtitle2" gutterBottom>{job.location}</Typography>
+                <div className='css-1ewacfc'> <Typography sx={{ fontSize: "9px", fontWeight: 400 }}>⏳ Posted 10 hours ago</Typography>
+                </div>
+                <Typography variant="h6" >{job.jobRole}</Typography>
+                <Typography sx={{fontSize:"11px"}} gutterBottom>{job.location}</Typography>
+                <Typography sx={{fontSize:"1rem"}} gutterBottom>About Company:</Typography>
                 <Typography variant="body2" color="textSecondary" paragraph>
                     {expanded ? job.jobDetailsFromCompany : `${job.jobDetailsFromCompany.slice(0, charactersLimit)}...`}
                     {!expanded && <Button onClick={toggle}>Read more</Button>}
                 </Typography>
-                <Typography variant="body2" gutterBottom>{`${job.minExp}Min - ${job.maxExp}Max yrs of Experience`}</Typography>
-                <Button href={job.jdLink} variant="contained" style={{ backgroundColor: '#67F0C5' }}>Apply</Button>
+                <Typography sx={{fontSize:"0.8rem"}} gutterBottom>Minimum Experience</Typography>
+                <Typography variant="body2" sx={{mb:2}}>{job.minExp||0} years</Typography>
+                <Button href={job.jdLink} variant="contained" sx={{ backgroundColor: '#67F0C5', width: "100%", color: "black", fontWeight: 500 }}> ⚡Easy Apply</Button>
             </CardContent>
         </Card>
     );
